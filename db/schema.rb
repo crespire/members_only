47f8491e1cb7b3d22e1a43ac9c48f6f008785738
crespire <li.simmon@gmail.com>
2022-02-11 15:10:09 -0500

    Update devise to allow login via username or email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_193334) do
+ActiveRecord::Schema.define(version: 2022_02_11_195148) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_02_11_193334) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
